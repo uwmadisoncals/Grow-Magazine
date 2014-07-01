04b6371bfa658f19c938fd76aeaea5c33c7825f7
Al Nemec <al.nemec@gmail.com>
2014-07-01 10:59:17 -0500

    Added new gulp config to grow website

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    newer = require('gulp-newer'),
+    livereload = require('gulp-livereload'),
+    lr = require('tiny-lr'),
+    server = lr();
+
+// Styles
+gulp.task('styles', function() {
+  return gulp.src('sass/master.scss')
+    .pipe(sass({ style: 'expanded', }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(livereload(server))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src(['bower_components/jquery/dist/jquery.min.js','js/vendors/*.js','js/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(livereload(server))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Debug
+gulp.task('debug', function() {
+  return gulp.src(['js/*.js'])
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(notify({ message: 'Debug task complete' }));
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(livereload(server))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+
+
+// Clean
+gulp.task('clean', function() {
+  return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false})
+    .pipe(clean());
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Listen on port 35729
+  server.listen(35729, function (err) {
+    if (err) {
+      return console.log(err)
+    };
+
+    // Watch .scss files
+    gulp.watch('sass/**/*.scss', ['styles']);
+
+    // Watch .js files
+    gulp.watch('js/**/*.js', ['scripts']);
+
+    // Debug .js files
+    gulp.watch('js/*.js', ['debug']);
+
+    // Watch image files
+    gulp.watch('img/**/*', ['images']);
+
+
+
+
+  });
+
+});
--- header.php
+++ header.php
@@ -14,17 +14,17 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-  
-  
-  
+
+
+
   var fbchange = false;
-	        	
+
 	        	var timer = setInterval(function() {
 		        	var t = jQuery(".fb-share-button span:first-child").attr("style");
-		        	
+
 		        	if(t) {
-			        	
-		        	
+
+
 		        	if(t == "height: 60px; width: 200px; " || t == "height: 60px; width: 200px;" || t == "width: 200px; height: 60px;") {
 			        	//switch to box
 			        	//console.log("switch to");
@@ -32,7 +32,7 @@
 				        	var ifcopy = jQuery(".fb-share-button span:first-child").html();
 				        	jQuery(".fb-share-button").parent().append("<div class='fb-share-copy'>"+ifcopy+"</div>");
 				        	jQuery(".fb-share-button").parent().append("<div class='fb-share-fbutton'></div>");
-				        	
+
 				        	jQuery(".fb-share-button").addClass("switch");
 				        	fbchange = true;
 			        	}
@@ -46,12 +46,12 @@
 			        	jQuery(".fb-share-copy").hide();
 			        	clearInterval(timer);
 		        	}
-		        	
+
 		        	}
 	        	},200);
 
 </script>
-		
+
 		<title>
 			<?php if (is_front_page()) { echo bloginfo('name');
 			} elseif (is_404()) {
@@ -71,28 +71,29 @@
 	    <meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 		<meta name="description" content="<?php bloginfo('description') ?>" />
 		<?php if(is_search()) { ?>
-		<meta name="robots" content="noindex, nofollow" /> 
+		<meta name="robots" content="noindex, nofollow" />
 	    <?php }?>
-	
+
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory');?>/style.css" media="print" />
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory');?>/style.css" media="screen" />
+				<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory');?>/dist/styles/master.css" />
 		<!--[if IE 7]>
 			<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/style/css/ie.css" media="screen" />
 		<![endif]-->
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	
-		<?php 
-		
+
+		<?php
+
 		//wp_head();
 		global $wpjsl;
 		$wpjsl = split_wp_head();
-				
+
 		?>
-	
+
 
     <meta name="keywords" content= "Grow Magazine, Life Sciences, UW-Madison, University of Wisconsin-Madison, College of Agricultural and Life Sciences, CALS, Wisconsin, Agriculture, Agricultural policy and reform, Alumni, Animal Health, Bacteriology, Biochemistry, Bioenergy, Cheese, Conservation, Dairy, Farming, Fermentation, Food and drink, Food crops, Food science, Forest ecology, Fruit, Genomics, Horticulture, Human-wildlife interactions, Instruction, International research, Internships, Landscape architecture, Microbiology, Nutrient management, Organics, Pathogens, Pesticides, Plant breeding,  Restoration, Soil science, Students, Sustainable agriculture, Water quality, Wildlife ecology" />
-    
+
     <meta name="description" content="Grow, Wisconsin's Magazine for the Life Sciences, is a magazine published by the University of Wisconsin-Madison College of Agricultural and Life Sciences. We report on the most intriguing aspects of CALS research, teaching and outreach in the areas of food and agriculture, health, energy, the environment and community development." />
 	</head>
 
@@ -115,18 +116,18 @@
             </div>
             <h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
 			<p><?php bloginfo('description'); ?></p>
-			
+
             <div id="nav2">
 				<ul >
-                   <?php  
-				   
+                   <?php
+
 				   $categories = array(3,4,5,6,7,8);
 				   foreach ($categories as $cat){?>
                     <li class="cat-item cat-item-<?php echo $cat;?> <?php if(is_category($cat)){ echo " current-cat";}?>" >
                     	<a href="<?php echo get_category_link($cat);?>" title="View all posts filed under <?php echo get_cat_name($cat);?>"><?php echo get_cat_name($cat);?></a>
                         <ul class="children">
 	                     <?php
-                         
+
 						//query_posts('showposts=5&orderby=date&order=DESC&cat='.$cat);
 						$my_query = new WP_Query('showposts=5&orderby=date&order=DESC&cat='.$cat);
 						while ($my_query->have_posts()) : $my_query->the_post();
@@ -134,12 +135,12 @@
     							<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
  						 <?php endwhile;?>
 
-                        		
+
                         </ul>
                     </li>
-			<?php 	
-					} ?> 
-					<li><a href="http://grow.cals.wisc.edu/files/2014/06/GrowSUM14-web.pdf" class="download_issue"  title="Download current issue">Download</a></li>  
+			<?php
+					} ?>
+					<li><a href="http://grow.cals.wisc.edu/files/2014/06/GrowSUM14-web.pdf" class="download_issue"  title="Download current issue">Download</a></li>
 				</ul>
 
 			</div>
